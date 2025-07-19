7062e9e7df793065929781129aad25cb85d7d5e6
rchowd9 <135185247+rchowd9@users.noreply.github.com>
2025-07-18 20:36:30 -0400

    Update textgames.hpp

--- textgames.hpp
+++ textgames.hpp
@@ -1,107 +1,51 @@
-#include "textgames.cpp"
+#ifndef TEXTGAMES_HPP
+#define TEXTGAMES_HPP
 
-GameObject::GameObject(int sizeX, int sizeY)
+#include <vector>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <cctype>
+#include <queue>
+#include <fstream> // NEW
+
+const int EMPTY_SPACE = 0;
+const int WALL = 2;
+const int PLAYER = 3;
+const int TARGET = 4;
+const int COLLECTIBLE = 5;
+
+class GameObject
 {
-    sizeGridX = sizeX;
-    sizeGridY = sizeY;
-    grid.resize(sizeGridY, vector<int>(sizeGridX, EMPTY_SPACE));
+private:
+    std::vector<std::vector<int>> grid;
+    int sizeGridX, sizeGridY;
+    int PlayerX, playerY;
+    int goalX, goalY;
 
-    PlayerX = 0;
-    playerY = 0;
-    goalX = sizeGridX - 1;
-    goalY = sizeGridY - 1;
+    int moveCount;
+    int health;
+    int score;
 
-    grid[goalY][goalX] = TARGET;
-}
+    bool isPathExist(int startX, int startY, int endX, int endY);
 
-void GameObject::display()
-{
-    for (int y = 0; y < sizeGridY; y++)
-    {
-        for (int x = 0; x < sizeGridX; x++)
-        {
-            if (x == PlayerX && y == playerY)
-                cout << "P ";
-            else if (grid[y][x] == TARGET)
-                cout << "T ";
-            else if (grid[y][x] == WALL)
-                cout << "# ";
-            else
-                cout << ". ";
-        }
-        cout << endl;
-    }
-}
+public:
+    GameObject(int sizeX, int sizeY);
+    void display();
+    void placeObstacles(int numObstacles, int numCollectibles);
+    void movePlayer(char direction);
 
-void GameObject::placeObstacles(int numObstacles)
-{
-    srand(time(0));
-    int placement = 0;
-    while (placement < numObstacles)
-    {
-        int x = rand() % sizeGridX;
-        int y = rand() % sizeGridY;
-        if (grid[y][x] == EMPTY_SPACE && !(x == PlayerX && y == playerY) && !(x == goalX && y == goalY))
-        {
-            grid[y][x] = WALL;
-            placement++;
-        }
-	}
-}
+    int getPlayerX() const { return PlayerX; }
+    int getPlayerY() const { return playerY; }
+    int getGoalX() const { return goalX; }
+    int getGoalY() const { return goalY; }
+    int getMoveCount() const { return moveCount; }
+    int getHealth() const { return health; }
+    int getScore() const { return score; }
 
-void GameObject::movePlayer(char direction)
-{
-    int newX = PlayerX;
-    int newY = playerY;
+    // NEW: Save/Load
+    bool saveToFile(const std::string& filename);
+    bool loadFromFile(const std::string& filename);
+};
 
-    if (direction == 'w')
-    {
-        newY--;
-    }
-
-    else if (direction == 's')
-    {
-        newY++;
-    }
-
-    else if (direction == 'a')
-    {
-        newX--;
-    }
-
-    else if (direction == 'd')
-    {
-        newX++;
-	}
-
-    else
-    {
-        cout << "Invalid move. Use 'w', 'a', 's', 'd' to move." << endl;
-        return;
-    }
-
-    if (newX < 0 || newX >= sizeGridX || newY < 0 || newY >= sizeGridY)
-    {
-        cout << "Move out of bounds!" << endl;
-        return;
-	}
-
-    if( grid[newY][newX] == WALL)
-    {
-        cout << "You hit a wall!" << endl;
-        return;
-	}
-
-    PlayerX = newX;
-    playerY = newY;
-
-    if (PlayerX == goalX && playerY == goalY)
-    {
-        cout << "Congratulations! You've reached the target!" << endl;
-    }
-
-    else
-    {
-        cout << "You moved to (" << PlayerX << ", " << playerY << ")." << endl;
-	}
-}
+#endif
